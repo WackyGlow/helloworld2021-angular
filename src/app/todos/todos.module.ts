a484afe30e7861e025bc5d8af179e7c575074801
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2021-10-13 12:24:06 +0200

    more plumbing

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodosRoutingModule } from './todos-routing.module';
+import { TodosOverviewComponent } from './todos-overview/todos-overview.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodosOverviewComponent
+  ],
   imports: [
     CommonModule,
     TodosRoutingModule
